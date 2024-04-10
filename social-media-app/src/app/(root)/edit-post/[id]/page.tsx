f0b1a4d6df30b06dbc9290c8b8517e55d6d8bdcd
Diego Engelhard <00356119@uca.edu.sv>
2024-04-10 15:20:59 -0600

    feat: edit post

--- social-media-app/src/app/(root)/edit-post/[id]/page.tsx
+++ social-media-app/src/app/(root)/edit-post/[id]/page.tsx
@@ -4,23 +4,36 @@ import { getServerSession } from "next-auth/next"
 import { options } from '@/app/api/auth/[...nextauth]/options';
 
 import { fetchUser } from '@/lib/actions/user.actions';
+import { fetchPostById } from '@/lib/actions/post.action';
 
 import PostForm from '@/components/forms/PostForm';
 
-const page = async () => {
+const page = async ({ params }: { params: { id: string } }) => {
+    // Check if params exist
+    if (!params.id) redirect('/');
+
     // Get next-auth session
     const session = await getServerSession(options)
 
-    // Redirect if no session
     if (!session) redirect('/');
 
     // Fetch user data
     const user = await fetchUser(session?.user?.email || '');
 
+    // Fetch post data
+    const post = await fetchPostById(params.id); // fetch post by id from db
+    if (!post) return null;
+
+    // Define post data
+    const postData = {
+        id: post._id,
+        text: post.text,
+    }
+
     return (
         <>
-            <h1 className='head-text'>Create Post</h1>
-            <PostForm userId={user._id} />
+            <h1 className='head-text'>Edit Post</h1>
+            <PostForm userId={user._id} postData={postData} edit={true} />
         </>
     )
 }
